ca9282245869901f8c68ad147213be4216ef5adf
raoulrubin <raoul@rubincentral.com>
2023-01-18 15:19:21 -0500

    remove build files

--- build/lib/code/markdown.py
+++
@@ -1,9 +0,0 @@
-""" Markdown formatting definitions """
-from typing import List
-
-entryTypes = {"note": 1}
-formats = {"markdown": 1}
-
-
-def tableToMarkdown(title: str, data: List, headers: List):
-    return 'markdown'
