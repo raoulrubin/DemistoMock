26f0f52729cacd09a9f726a9c9f826215a480c1e
raoulrubin <raoul@rubincentral.com>
2023-01-18 13:45:41 -0500

    Create Files

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(name='DemistoMock',
+version='0.2',
+description='Demisto mock tools package',
+url='#',
+author='Raoul',
+install_requires=['opencv-python'],
+author_email='william.rubin@fmc-na.com',
+packages=setuptools.find_packages(),
+zip_safe=False)
+
+# pip install .
